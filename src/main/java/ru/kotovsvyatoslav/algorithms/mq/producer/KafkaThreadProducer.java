41a3366efe1fe0f97ee98c155550506a42685c4f
Svyatoslav Kotov <kotov.svyat@mail.ru>
2023-02-01 11:17:29 +0300

    add kafka producer multithreading

---
+++ src/main/java/ru/kotovsvyatoslav/algorithms/mq/producer/KafkaThreadProducer.java
@@ -0,0 +1,82 @@
+package ru.kotovsvyatoslav.algorithms.mq.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Queue;
+
+public class KafkaThreadProducer extends Thread{
+
+    private Queue<String> msgQueue = new ArrayDeque<>();
+    private String sessionId;
+    private String topic;
+    private boolean sorting;
+
+    private KafkaProducer kafkaProducer;
+
+    public KafkaThreadProducer(KafkaProducer kafkaProducer, String topic, String sessionId) {
+        this.kafkaProducer = kafkaProducer;
+        this.topic = topic;
+        this.sessionId = sessionId;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("producer start");
+        System.out.println(sorting);
+        sorting=true;
+        boolean producing = true;
+        while (producing){
+            if (msgQueue.isEmpty() && sorting){
+                try {
+                    synchronized (this){
+                        System.out.println("producer wait");
+                        this.wait();
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }else if (msgQueue.isEmpty() && !sorting){
+                System.out.println("produce end");
+                producing = false;
+            }else {
+                System.out.println("producer post");
+                HashMap<String, String> msgMag = new HashMap<>();
+                msgMag.put("sessionId", sessionId);
+                msgMag.put("message", msgQueue.poll());
+
+                kafkaProducer.produce(topic, msgMag);
+            }
+
+        }
+        System.out.println("producer end");
+
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void setSorting(boolean sorting) {
+        this.sorting = sorting;
+    }
+    public void addQueue (String msg) {
+        this.msgQueue.add(msg);
+    }
+
+    public Queue<String> getMsgQueue() {
+        return msgQueue;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public void setKafkaProducer(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+}
+
